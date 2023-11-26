e77bb8b73803b16cda918938798a31f8ba40a256
cruldra <cruldra@gmail.com>
2023-11-26 23:45:15 +0800

    fix: 修复打包出来的文件名有可能被放到二级目录下的问题

--- template-cleanup.js
+++ template-cleanup.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const fs = require('fs');
-const Handlebars = require('handlebars');
+import path from 'path'
+import fs from 'fs'
+import Handlebars from 'handlebars'
 
 // 获取当前命令行上下文路径
 const currentDirectory = process.cwd();
@@ -9,8 +9,12 @@ const packageJsonPath = path.join(currentDirectory, 'package.json');
 const packageJsonFile = fs.readFileSync(packageJsonPath, 'utf8');
 const viteConfigPath = path.join(currentDirectory, 'vite.config.ts');
 const viteConfigFile =  fs.readFileSync(viteConfigPath, 'utf8');
+const preReleaseJsPath = path.join(currentDirectory, '.github/scripts/pre-release.js');
+const preReleaseJsFile =  fs.readFileSync(preReleaseJsPath, 'utf8');
 const packageJsonTemplate = Handlebars.compile(packageJsonFile);
 const viteConfigTemplate = Handlebars.compile(viteConfigFile);
+const preReleaseJsTemplate = Handlebars.compile(preReleaseJsFile);
+
 const data = {
     repo_name: process.argv[2],
     author: process.argv[3],
@@ -20,5 +24,7 @@ const data = {
 };
 const output = packageJsonTemplate(data);
 const output2 = viteConfigTemplate(data);
+const output3 = preReleaseJsTemplate(data);
 fs.writeFileSync(packageJsonPath, output);
 fs.writeFileSync(viteConfigPath, output2);
+fs.writeFileSync(preReleaseJsPath, output3);
