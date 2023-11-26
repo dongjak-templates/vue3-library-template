5cf7b7d4ddcfff86761d29ae7e6e92ac4a9f04f1
cruldra <cruldra@gmail.com>
2023-11-26 23:20:58 +0800

    fix: 修复打包出来的文件名有可能被放到二级目录下的问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import path from "path";
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite';
-
 export default defineConfig({
     plugins: [
         vue(),
